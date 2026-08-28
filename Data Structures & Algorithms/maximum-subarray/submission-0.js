941be720d4acaa127635224759785523f9e782be
Konstantin Azarov <megareto667@gmail.com>
2026-08-28 13:40:15 -0400

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let current = nums[0];
+        let best = nums[0];
+
+        for (let i = 1; i < nums.length; i++) {
+            current = Math.max(nums[i], current + nums[i]);
+            best = Math.max(best, current);
+        }
+
+        return best;
+    }
+}
