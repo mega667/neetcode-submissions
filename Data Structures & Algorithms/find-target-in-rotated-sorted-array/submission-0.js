53fb426a84e6f545ea4995ca05f82076e137504a
Konstantin Azarov <megareto667@gmail.com>
2026-08-28 13:16:13 -0400

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            const mid = Math.floor((l + r) / 2);
+
+            if (nums[mid] === target) {
+                return mid;
+            }
+
+            if (nums[l] <= nums[mid]) {
+                // Left side sorted
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+
+            } else {
+                // Right side sorted
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+
+            }
+        }
+
+        return -1;
+    }
+}
