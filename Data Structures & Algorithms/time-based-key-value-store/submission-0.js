1b563d7ed4afa26042c0856868ac3eabc3dac94b
Konstantin Azarov <megareto667@gmail.com>
2026-08-30 16:15:38 -0400

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.js
@@ -0,0 +1,46 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if (!this.keyStore.has(key)) {
+            this.keyStore.set(key, []);
+        }
+        this.keyStore.get(key).push([value, timestamp]);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        if (!this.keyStore.has(key)) return '';
+
+        const values = this.keyStore.get(key);
+        let left = 0;
+        let right = values.length - 1;
+        let result = '';
+
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            const [value, currentTimestamp] = values[mid];
+            if (currentTimestamp <= timestamp) {
+                result = value;
+
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
